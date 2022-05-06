b620203ae669a884963046d878259765dc41d406
FEDEREGOROV <100992312+FEDEREGOROV@users.noreply.github.com>
2022-05-06 15:45:43 +0300

    prak2Eg

---
+++ rumireaEGorovpractice2/dialog/src/main/java/ru/mirea/egorov/dialog/MyProgressDialogFragment.java
@@ -0,0 +1,20 @@
+package ru.mirea.egorov.dialog;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+import androidx.fragment.app.DialogFragment;
+
+public class MyProgressDialogFragment extends DialogFragment {
+    Context context;
+    public MyProgressDialogFragment(Context context){
+        this.context = context;
+    }
+
+    public void show(){
+        ProgressDialog progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Идёт загрузка, подождите.");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+    }
+}
