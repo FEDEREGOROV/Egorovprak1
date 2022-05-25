6d63a319a2543c84c95a12b97642962e58b5ae5b
FEDEREGOROV <100992312+FEDEREGOROV@users.noreply.github.com>
2022-05-25 18:35:11 +0300

    prakEg

---
+++ rumireaegorovpractice4/thread/src/main/java/ru/mirea/egorov/thread/thread.java
@@ -0,0 +1,14 @@
+package ru.mirea.egorov.thread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thread extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread);
+    }
+}
